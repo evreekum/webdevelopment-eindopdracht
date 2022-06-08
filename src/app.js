64fffde512583a6a54c269c2cf5b23f0b837a6a6
evreekum <e.v.reekum@gmail.com>
2022-06-08 19:00:18 +0200

    Started on CSS, cleaning up HTML

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ import axios from "axios";
 
 // axios.get('https://api.edamam.com/api/recipes/v2');
 
-async function getDataRecipes() {
+/*async function getDataRecipes(searchQuery) {
     try {
-        const response = await axios.get('https://api.edamam.com/api/recipes/v2');
+        const response = await axios.get('https://api.edamam.com/api/recipes/v2'), config: {
+            params: {
+                type: 'public',
+                app_key: '',
+                app_id: '',
+                q: searchQuery
+
+            }
+        }
         console.log(response);
     } catch (e) {
         console.error(e);
     }
 }
-getDataRecipes();
+getDataRecipes();*/
 
 
-async function getFoods() {
+async function getDataFoods() {
     try {
         const response = await axios.get();
         console.log(response);
